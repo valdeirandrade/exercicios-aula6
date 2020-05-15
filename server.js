7d56a49593f910a97cfb3075950564b10729e89a
valdeirandrade <andrade_mai@yahoo.com.br>
2020-05-15 19:19:28 -0400

    preparando para heroku

---
+++ server.js
@@ -0,0 +1,10 @@
+const express = require("express")
+let app = express();
+
+app.listen(process.env.PORT);
+
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 8000;
+}
+app.listen(port);
