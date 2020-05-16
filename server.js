20309bd48c3439883fbdbf192c637ccb8bd22004
valdeirandrade <andrade_mai@yahoo.com.br>
2020-05-15 20:22:07 -0400

    js teste

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 const express = require("express")
 let app = express();
-app.listen(process.env.PORT);
-let port = process.env.PORT
+app.use(express.static('public'));
+let port = process.env.PORT;
 if (port == null || port == "") {
-  port = 8000;
+    port = 8000;
 }
 app.listen(port);
